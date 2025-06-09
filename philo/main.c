ce034307b06bbe4d8d827615f1bafaaa087f30a6
Acholias <mugot.lucas@gmail.com>
2025-06-09 18:19:19 +0200

    Add meals_count for stop routine

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: lumugot <lumugot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 10:10:21 by lumugot           #+#    #+#             */
-/*   Updated: 2025/06/09 17:41:12 by lumugot          ###   ########.fr       */
+/*   Updated: 2025/06/09 18:18:58 by lumugot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,36 @@ void	*monitor_routine(void *arg)
 {
     t_philo	*philos = (t_philo *)arg;
     t_data	*data = philos[0].data;
-    int			i;
+    int		i;
+    int		full;
     long int	last_meal;
 
     while (!data->simu_off)
     {
+        full = 0;
         i = 0;
         while (i < data->nb_philos)
         {
             pthread_mutex_lock(&philos[i].meal_mutex);
             last_meal = philos[i].last_meal;
+            if (data->nb_meals > 0 && philos[i].nb_meals_eaten >= data->nb_meals)
+                full++;
             pthread_mutex_unlock(&philos[i].meal_mutex);
             if ((get_time() - last_meal) >= data->t_die)
             {
                 pthread_mutex_lock(&data->print_mutex);
                 if (!data->simu_off)
-                    printf("%ld %d died\n", get_time() - data->start_time, philos[i].id + 1);
+                    printf("%ld %d died\n", get_time() - data->start_time, philos[i].id);
                 data->simu_off = 1;
                 pthread_mutex_unlock(&data->print_mutex);
                 return (NULL);
             }
             i++;
+        }
+        if (data->nb_meals > 0 && full == data->nb_philos)
+        {
+            data->simu_off = 1;
+            return (NULL);
         }
         usleep(1000);
     }
